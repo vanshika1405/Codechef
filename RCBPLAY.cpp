8cf57cb7f0a6a0d1ff1a8fff8f541ef3455918c7
Vanshika Sharma <87427083+vanshika1405@users.noreply.github.com>
2022-04-19 22:28:41 +0530

    Add files via upload

---
+++ RCBPLAY.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t;
+   cin>>t;
+   while(t-->0){
+       int x,y,z,t,i;
+       cin>>x>>y>>z;
+       t=z*2;
+      i=t+x;
+      if(i>=y){
+          cout<<"yes";
+      }
+   
+       else{
+           cout<<"no";
+       }
+ {cout<<endl;}  }
+    
+}
+ 
